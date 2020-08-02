fd81cf59f8d045954bd652e0a3caa62d0d7835cd
an hong <hongan529@gmail.com>
2020-08-02 10:17:51 +0700

    Add project files.

---
+++ WebBootstrap_Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebBootstrap_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
